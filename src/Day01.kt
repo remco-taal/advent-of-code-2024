b51c61cf3dc6ccb7193130595dd3a14647a9e022
PC-REMCO\Remco <remcotaal@hotmail.com>
2024-12-01 09:22:02 +0100

    Add solution Day01 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,31 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftLocationIds = mutableListOf<Int>()
+        val rightLocationIds = mutableListOf<Int>()
+        input.forEach { line ->
+            val locationIds = line.split("   ")
+            leftLocationIds.add(locationIds.first().toInt())
+            rightLocationIds.add(locationIds.last().toInt())
+        }
+
+        leftLocationIds.sort()
+        rightLocationIds.sort()
+
+        return leftLocationIds.withIndex().sumOf { (index, leftLocationId) ->
+            val distance = (leftLocationId - rightLocationIds[index]).absoluteValue
+            return@sumOf distance
+        }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")

e140dd61c2c6b1abb099d0e40d17e3ab063a0247
PC-REMCO\Remco <remcotaal@hotmail.com>
2024-12-07 10:45:55 +0100

    Remove comments Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -20,11 +20,9 @@ fun main() {
         }
     }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
